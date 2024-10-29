d4bf66e2789b7afd90e5a39842d3af39d6bcd7b3
herman pietrus <alldjimch@gmail.com>
2024-10-29 09:12:31 +0100

    My-blog

---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+  {
+    id: 1,
+    title: '7 CSS tools you should be using ',
+    category: 'development',
+    subCategory: ['frontend', 'ui/ux', 'design'],
+    description:
+      "CSS3 has been the standard for a while, and it brought a lot of cool features to the table. Now, with CSS7, the evolution continues with even more advanced capabilities for web designers and developers. Here’s how CSS7 can be beneficial make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/designer-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Planification éfficace de Projets',
+    category: 'Project Plan',
+    subCategory: ['Brainstorming', 'Planification', 'Action'],
+    description:
+      "La gestion de projet, c'est tout un art ! Elle consiste à planifier, organiser et gérer des ressources pour atteindre des objectifs spécifiques dans un délai imparti. Planification : Une bonne gestion de projet commence par une planification (recherche de solutions) minutieuse. Il s'agit de définir les objectifs, les étapes clés, les délais et les ressources nécessaires.Organisation : Organiser les ressources, qu'elles soient humaines, financières ou matérielles, est crucial pour mener à bien le projet. Cela inclut la répartition des tâches et la gestion des équipes.Organisation : Organiser les ressources, qu'elles soient humaines, financières ou matérielles, est crucial pour mener à bien le projet. Cela inclut la répartition des tâches et la gestion des équipes.Organisation : Organiser les ressources, qu'elles soient humaines, financières ou matérielles, est crucial pour mener à bien le projet. Cela inclut la répartition des tâches et la gestion des équipes.Organisation : Organiser les ressources, qu'elles soient humaines, financières ou matérielles, est crucial pour mener à bien le projet. Cela inclut la répartition des tâches et la gestion des équipes.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/Project_plan.jpg',
+  },
+  {
+    id: 3,
+    title: 'Manage SEO and get attendance',
+    category: 'SEA/SEO',
+    subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+  },
+  {
+    id: 4,
+    title: 'Deep code reading',
+    category: 'adventure',
+    subCategory: ['Frontend', 'Backend'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/programming80.jpg',
+  },
+  {
+    id: 5,
+    title: 'Ingenius idea for being a good programmer',
+    category: 'cooking',
+    subCategory: [ 'Plan', 'code'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/coder.jpg',
+  },
+  {
+    id: 6,
+    title: 'Learn by yourself how code works',
+    category: 'Pratice',
+    subCategory: ['JS', 'laravel', 'design'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2024',
+    cover:
+      '/assets/images/pratice.jpg',
+  },
+  {
+    id: 7,
+    title: 'FAQ',
+    category: 'Get answser from all your questions',
+    subCategory: ['skill', 'design', 'passion'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    authorName: 'Herman Pietrus',
+    authorAvatar: '/assets/images/author.jpg',
+    createdAt: 'June 03, 2021',
+    cover: '/assets/images/FAQ.png',
+  },
+];
